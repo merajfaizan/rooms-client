2ef99535e7bbd57e0377d8519532053271b05721
Meraj Faizan <mdmaraj1500@gmail.com>
2023-12-04 11:17:06 +0600

    updated room page with filter funtion

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -6,8 +6,9 @@ const Rooms = () => {
   const axiosPublic = useAxiosPublic();
 
   const [rooms, setRooms] = useState([]);
-  const [minPrice, setMinPrice] = useState("");
-  const [maxPrice, setMaxPrice] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(1000);
 
   useEffect(() => {
     // Fetch rooms based on the current filter values
@@ -23,6 +24,8 @@ const Rooms = () => {
         setRooms(response.data);
       } catch (error) {
         console.error("Error fetching rooms:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -31,20 +34,55 @@ const Rooms = () => {
   }, [minPrice, maxPrice, axiosPublic]);
 
   return (
-    <div className="container mx-auto my-8">
+    <div className="px-5 my-8">
       <h2 className="text-3xl font-bold mb-4">Available Rooms</h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {rooms.map((room) => (
-          <RoomCard key={room._id} room={room} />
-        ))}
+      {/* Filter section */}
+      <div className="mb-4">
+        <label className="text-lg font-bold mb-2">Filter by Price Range:</label>
+        <div className="flex items-center">
+          <div className="flex-1 mr-4">
+            <label htmlFor="minPrice" className="block text-sm text-gray-600">
+              Min Price: ${minPrice}
+            </label>
+            <input
+              id="minPrice"
+              type="range"
+              min={0}
+              max={1000}
+              step={1}
+              value={minPrice}
+              onChange={(e) => setMinPrice(parseInt(e.target.value))}
+              className="range"
+            />
+          </div>
+          <div className="flex-1">
+            <label htmlFor="maxPrice" className="block text-sm text-gray-600">
+              Max Price: ${maxPrice}
+            </label>
+            <input
+              id="maxPrice"
+              type="range"
+              min={0}
+              max={1000}
+              step={1}
+              value={maxPrice}
+              onChange={(e) => setMaxPrice(parseInt(e.target.value))}
+              className="range"
+            />
+          </div>
+        </div>
       </div>
 
-      {/* Price filter - You can implement this as per your requirements */}
-      <div className="mt-8">
-        <label className="text-lg font-bold">Filter by Price Range:</label>
-        {/* Add your price range filter component here */}
-      </div>
+      {loading ? (
+        <span className="loading loading-spinner loading-lg"></span>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {rooms.map((room) => (
+            <RoomCard key={room._id} room={room} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
